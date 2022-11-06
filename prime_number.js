bec98683b272b9cf04c85ae06cbe413ac1775582
abdulrashid06 <abdulrashid987655@gmail.com>
2022-11-06 21:15:01 +0530

    prime number code written

--- prime_number.js
+++ prime_number.js
@@ -1,10 +1,10 @@
     let factor=0;
 
     for (let i=1; i<=num; i++){
-         if (num%i==0){
+         if (num%i==2){
             factor++;
          }
-    }if (factor==2){
+    }if (factor==0){
         console.log("Yes");
     }else{
         console.log("No");
